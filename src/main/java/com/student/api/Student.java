d7d272479e4e41dc857cf3c3b7f34f96e3737f7c
arunbbm <arunbm.mca15@gmail.com>
2020-01-25 10:53:39 +0530

    initial commit

---
+++ src/main/java/com/student/api/Student.java
@@ -0,0 +1,48 @@
+package com.student.api;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String branch;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student(firstName,lastName,email,branch) VALUES('arun','math','arunbmath@gmail.com','MCA');
+INSERT INTO student(firstName,lastName,email,branch) VALUES('anil','kapoor','anilkapoor@gmail.com','BE');
+COMMIT;
